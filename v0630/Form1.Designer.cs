c1669f1e55a5915904b37fa060d13435357ffe95
Toya <66713857+NorauDn@users.noreply.github.com>
2022-06-30 15:36:08 +0900

    圧倒的力技

--- v0630/Form1.Designer.cs
+++ v0630/Form1.Designer.cs
@@ -34,16 +34,19 @@ namespace v0630
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timer1
             // 
+            this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 32);
+            this.label1.Location = new System.Drawing.Point(-3, -2);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(19, 15);
             this.label1.TabIndex = 0;
@@ -53,11 +56,30 @@ namespace v0630
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(54, 9);
+            this.label2.Location = new System.Drawing.Point(362, 288);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(37, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "(・ω・)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, -2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "★";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, -2);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "★";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Form1
             // 
@@ -65,7 +87,9 @@ namespace v0630
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(120, 49);
+            this.ClientSize = new System.Drawing.Size(808, 573);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -81,6 +105,8 @@ namespace v0630
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -12,7 +12,9 @@ namespace v0630
 {
     public partial class Form1 : Form
     {
-        int vx= rand.Next(-10, 11), vy= rand.Next(-10, 11);
+        int vx1= rand.Next(-10, 11), vy1= rand.Next(-10, 11);
+        int vx2 = rand.Next(-10, 11), vy2 = rand.Next(-10, 11);
+        int vx3 = rand.Next(-10, 11), vy3 = rand.Next(-10, 11);
         //静的（最初に決めておく）　static　↔動的（実行時に変更可能）
         static Random rand = new Random();
 
@@ -22,6 +24,10 @@ namespace v0630
 
             label1.Left = rand.Next(ClientSize.Width- label1.Width);
             label1.Top = rand.Next(ClientSize.Height - label1.Height);
+            label3.Left = rand.Next(ClientSize.Width - label3.Width);
+            label3.Top = rand.Next(ClientSize.Height - label3.Height);
+            label4.Left = rand.Next(ClientSize.Width - label4.Width);
+            label4.Top = rand.Next(ClientSize.Height - label4.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -31,25 +37,82 @@ namespace v0630
             label2.Left = fpos.X - label2.Width / 2; 
             label2.Top = fpos.Y - label2.Height / 2;
 
-            label1.Left += vx;
-            label1.Top += vy;
+            label1.Left += vx1;
+            label1.Top += vy1;
+            label3.Left += vx2;
+            label3.Top += vy2;
+            label4.Left += vx3;
+            label4.Top += vy3;
 
-           
+
 
             if (label1.Top < 0 ){
-                vy = Math.Abs(vy);
+                vy1 = Math.Abs(vy1);
             }
             if (label1.Top > ClientSize.Height) {
-                vy = -vy;
+                vy1 = -vy1;
             }
             
             if (label1.Left < 0 ) {
-                vx = Math.Abs(vx);
+                vx1 = Math.Abs(vx1);
               
             }
             if (label1.Left > ClientSize.Width) {
-                vx = -vx;
+                vx1 = -vx1;
             }
+
+
+            if (label3.Top < 0)
+            {
+                vy2 = Math.Abs(vy2);
+            }
+            if (label3.Top > ClientSize.Height)
+            {
+                vy2 = -vy2;
+            }
+
+            if (label3.Left < 0)
+            {
+                vx2 = Math.Abs(vx2);
+
+            }
+            if (label3.Left > ClientSize.Width)
+            {
+                vx2 = -vx2;
+            }
+
+
+            if (label4.Top < 0)
+            {
+                vy3 = Math.Abs(vy3);
+            }
+            if (label4.Top > ClientSize.Height)
+            {
+                vy3 = -vy3;
+            }
+
+            if (label4.Left < 0)
+            {
+                vx3 = Math.Abs(vx3);
+
+            }
+            if (label4.Left > ClientSize.Width)
+            {
+                vx3 = -vx3;
+            }
+
+            /*
+              
+             
+            ＿人人人 人人人＿
+            ＞  圧倒的力技 ＜
+            ￣^y^y^y^y^y^y^￣
+
+
+            */
+
+
+
 
             //vx=math.abs(vx);正の値を返す
 
@@ -58,6 +121,14 @@ namespace v0630
                  timer1.Enabled = false;
              }*/
             if (fpos.X <= label1.Right && fpos.Y <= label1.Bottom && fpos.X > label1.Left && fpos.Y > label1.Top)
+            {
+                timer1.Enabled = false;
+            }
+            if (fpos.X <= label3.Right && fpos.Y <= label3.Bottom && fpos.X > label3.Left && fpos.Y > label3.Top)
+            {
+                timer1.Enabled = false;
+            }
+            if (fpos.X <= label4.Right && fpos.Y <= label4.Bottom && fpos.X > label4.Left && fpos.Y > label4.Top)
             {
                 timer1.Enabled = false;
             }
@@ -76,6 +147,11 @@ namespace v0630
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
         {
 
         }
